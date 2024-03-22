5236bab1da5fabe5a541274f2dd642ba2a9efda3
kalangijayakody <kalangijayakody1002@gmail.com>
2024-03-22 22:47:58 +0530

    remove new module files

--- routes/newModule.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const newModuleController = require("../controllers/newModuleController");
-
-//router.post("/", newModuleController.handleNewModule);
-
-module.exports = router;
