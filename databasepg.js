8d1fff1d70f082c9a7920fd16bb2a1a50ad178ed
sahan <sahangawesh19@gmail.com>
2024-04-01 05:40:20 +0530

    secret leak fix

--- databasepg.js
+++ databasepg.js
@@ -1,11 +1,20 @@
 const { Client } = require('pg');
 
 const dbClient = new Client({
-    host: "34.69.240.11",
+    host: "localhost",
     user: "postgres",
     port: 5432,
-    password: "12345678",
+    password: "1234",
     database: "APGS"
 });
+
+
+// const dbClient = new Client({
+//     host: "34.69.240.11",
+//     user: "postgres",
+//     port: 5432,
+//     password: "12345678",
+//     database: "APGS"
+// });
 
 module.exports = dbClient;
