7b98a862b6941e30e7ec0ed6c223827fafe5c18a
sahan <sahangawesh19@gmail.com>
2024-05-22 13:28:38 +0530

    bug fix

--- controllers/assignmentController.js
+++ controllers/assignmentController.js
@@ -110,12 +110,12 @@ const Update =async (req,res)=>{
         if(title==null){
             return res.status(400).josn('assignment title cannot be null');
         }
-        await client.query('UPDATE assignments assignmenttitle= $1 WHERE modulecode = $2 batch=$3 assignmentid=$4',[title,modulecode,batch,id])
+        await client.query('UPDATE assignments SET assignmenttitle= $1 WHERE modulecode = $2 batch=$3 assignmentid=$4',[title,modulecode,batch,id])
         return res.status(200).json('successful');
     }
     catch (e){
         console.log(e);
-        return res.satus(400).json('Bad request');
+        return res.status(400).json('Bad request');
     }
 
 }
