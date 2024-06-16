36d807c0f88485b30239ea6e17312979bd8892bc
KalangiJayakody <thathsarakalangi@gmail.com>
2024-06-16 18:40:47 +0530

    commit

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,7 @@
 const allowedOrigins = [
-    
-    'http://127.0.0.1:5500',
-    'http://localhost:3500',
-    'http://localhost:3000'
-
+  "http://127.0.0.1:5500",
+  "http://localhost:3500",
+  "http://localhost:3000",
 ];
 
 module.exports = allowedOrigins;
