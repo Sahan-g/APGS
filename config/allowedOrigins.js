e08c7b987157a3603e1dff1c9539e9041d1342c0
Githmi Sundarasekara <githmisundarasekara@gmail.com>
2024-06-27 01:29:45 +0530

    commit

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     
     'http://127.0.0.1:5500',
     'http://localhost:3500',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'http://localhost:5000'
 
 ];
 
